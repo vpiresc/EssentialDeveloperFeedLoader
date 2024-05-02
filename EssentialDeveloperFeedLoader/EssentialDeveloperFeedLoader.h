0be0fb6380c9a3f46c13b1c3daefc255b9f36859
vitorpiresc <vitorpiresc@gmail.com>
2024-05-02 15:06:23 -0300

    Initial Commit

---
+++ EssentialDeveloperFeedLoader/EssentialDeveloperFeedLoader.h
@@ -0,0 +1,18 @@
+//
+//  EssentialDeveloperFeedLoader.h
+//  EssentialDeveloperFeedLoader
+//
+//  Created by Vitor Pires on 02/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialDeveloperFeedLoader.
+FOUNDATION_EXPORT double EssentialDeveloperFeedLoaderVersionNumber;
+
+//! Project version string for EssentialDeveloperFeedLoader.
+FOUNDATION_EXPORT const unsigned char EssentialDeveloperFeedLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialDeveloperFeedLoader/PublicHeader.h>
+
+
